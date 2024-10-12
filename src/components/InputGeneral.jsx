8080aa43bfc3e936d5f6b5fc5560e7002683f5f7
Luis Fernando <lferquint@gmail.com>
2024-10-11 20:31:54 -0600

    add secundaryDataSection

--- src/components/InputGeneral.jsx
+++ src/components/InputGeneral.jsx
@@ -1,7 +1,7 @@
 import '../assets/styles/InputGeneral.css'
 function Input({ labelText, typeInput='text'}){
     return(
-        <div>
+        <div className='container_input_general'>
             <label style={{minWidth: '100px', display: 'inline-block'}} htmlFor={labelText}>{labelText}</label>
             <input className='input_general' required type={typeInput} name={labelText} id={labelText} placeholder={labelText}/>
         </div>
--- src/components/SectionModifyProducts.jsx
+++ src/components/SectionModifyProducts.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react"
 import { useState } from "react"
+import SecundaryDataSection from "./SecundaryDataSection"
 
-function BasicInput({value, label}){
+function BasicInput({value, label, disabled}){
   const [inputValue, setInputValue] = useState(value)
   function handleChange(e){
     setInputValue(e.target.value)
@@ -9,40 +10,60 @@ function BasicInput({value, label}){
   return (
     <div>
       <label htmlFor={label} style={{display:'block'}}>{label}</label>
-      <input disabled type="text" onChange={handleChange} value={inputValue} id={label}></input>
+      <input disabled={disabled} type="text" onChange={handleChange} value={inputValue} id={label}></input>
     </div>
   )
 }
 
-function InputsModifyOnlyOneProduct({objConfig}){
+function InputsModifyOnlyOneProduct({objConfig, refresh, setRefresh}){
+  const [disabled, setDisabled] = useState(true)
+  function handleUpdate(){
+    setDisabled(!disabled)
+  }
+  function handleDelete(){
+    fetch(`http://localhost:3000/protected/deleteProduct/${objConfig.id_product}`, {
+      method: 'POST',
+      credentials: 'include'
+    })
+    setRefresh(!refresh)
+  }
   return (
     <>
     <div style={{display: 'flex', marginTop: '40px'}}>
-      {<BasicInput value={objConfig.type_product_name} label="Tipo de producto" />}
-      {<BasicInput value={objConfig.description} label="Descripcion"/>}
-      {<BasicInput value={objConfig.price} label="Precio"/>}
-      <input style={{alignSelf: 'flex-end'}} type="button" value="Actualizar" />
-      <input style={{alignSelf: 'flex-end'}} type="button" value="Borrar" />
+      {<BasicInput disabled value={objConfig.type_product_name} label="Tipo de producto" />}
+      {<BasicInput disabled value={objConfig.name_model} label="Model" />}
+      {<BasicInput disabled= {disabled} value={objConfig.price} label="Precio"/>}
+      {<BasicInput disabled= {disabled} value={objConfig.stock} label="Stock"/>}
+      <input type="hidden" value={objConfig.id_product}/>
+      <input onClick={handleUpdate} style={{alignSelf: 'flex-end', background:'blue'}} type="button" value="Actualizar" />
+      <input onClick={handleDelete} style={{alignSelf: 'flex-end', background: 'red'}} type="button" value="Borrar" />
     </div>
-    <input type="button" value="Guardar"/>
+    <div>
+      {
+        !disabled && <input type="button" value="Guardar"/>
+      }  
+    </div>    
     </>
   )
 }
 
 function SectionModifyProducts(){
   const [listProducts, setListProducts] = useState([])
+  const [refresh, setIsRefresh] = useState('')
   useEffect(()=>{
     fetch('http://localhost:3000/api/getAllProducts')
     .then((data)=>data.json())
-    .then((data)=>{setListProducts(data); console.log(data)})
-  }, [])
+    .then((data)=>{setListProducts(data)})
+  }, [refresh])
   return(
     <div>
       {
         listProducts.map((productObj)=>{
-          return <InputsModifyOnlyOneProduct key={productObj.id_product} objConfig={productObj} />
+          return <InputsModifyOnlyOneProduct refresh={refresh} setRefresh={setIsRefresh} key={productObj.id_product} objConfig={productObj} />
         })
       }
+      {/* <input type="button" value="Add product"/> */}
+    <SecundaryDataSection/>
     </div>
   )
 }
