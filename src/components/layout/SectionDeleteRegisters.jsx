7379ed346eb5e9c6ff5e22354618d20507d7c7bc
Fernando Quintana <lferquint@gmail.com>
2024-11-04 15:14:12 -0600

    Add form delete color

--- src/components/layout/SectionDeleteRegisters.jsx
+++ src/components/layout/SectionDeleteRegisters.jsx
@@ -1,4 +1,56 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import SectionGeneric from "./SectionGeneric"
+import FormBasic from "../common/FormBasic"
+import InputSubmit from "../common/InputSubmit"
+import TitleBasic from "../common/TitleBasic"
+import SelectLabelLeft from "../common/SelectLabelLeft"
+import ModalSuccessMessage from "../common/ModalSuccessMessage"
+
 function SectionDeleteRegisters(){
-  return <p>Aqui se van a borrar todos los registros</p>
+  const [listColors, setListColors] = useState([])
+  const [success, setSuccess] = useState(false)
+
+  function handleTimeSuccessMessage(){
+    setTimeout(()=>{ setSuccess(false) }, 4000)
+  }
+  function handleDelete(e){
+    e.preventDefault()
+    let array = Array.from(e.target)
+    array.pop()
+    const values = array.map((input)=> input.value)
+    fetch(`http://localhost:3000/protected/deleteColor/${values[0]}`, {
+      method: 'POST',
+      credentials: 'include'
+    }).then(()=>{ setSuccess(true); handleTimeSuccessMessage() })
+  }
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/api/getAllColors')
+    .then((res)=>res.json())
+    .then((data)=>{ setListColors(data) })
+  }, [])
+  
+  return (
+    <>
+      {
+        success ? <ModalSuccessMessage/> : ''
+      }
+      <SectionGeneric>
+        <TitleBasic text="Borrar color"/>
+        <FormBasic handleSubmit={handleDelete}>
+          <SelectLabelLeft 
+            labelText='Seleccionar color' 
+            options={ listColors }
+            optionValueIsId={true}
+            objContentKey="color_name"
+            objIdKey="id_color"
+          />
+          <InputSubmit text="Borrar"/>
+        </FormBasic>
+      </SectionGeneric>    
+    </>
+
+  )
 }
 export default SectionDeleteRegisters
