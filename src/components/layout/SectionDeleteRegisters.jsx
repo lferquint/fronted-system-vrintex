26ea9677aaa4133be8be701dceb62a1e0f02dcc0
Fernando Quintana <lferquint@gmail.com>
2024-11-04 17:26:07 -0600

    add 'Borrar tipo de producto' in process

--- src/components/layout/SectionDeleteRegisters.jsx
+++ src/components/layout/SectionDeleteRegisters.jsx
@@ -10,11 +10,12 @@ import ModalSuccessMessage from "../common/ModalSuccessMessage"
 function SectionDeleteRegisters(){
   const [listColors, setListColors] = useState([])
   const [success, setSuccess] = useState(false)
+  const [listTypeProduct, setListTypeProduct] = useState([])
 
   function handleTimeSuccessMessage(){
     setTimeout(()=>{ setSuccess(false) }, 4000)
   }
-  function handleDelete(e){
+  function handleDeleteColor(e){
     e.preventDefault()
     let array = Array.from(e.target)
     array.pop()
@@ -24,13 +25,23 @@ function SectionDeleteRegisters(){
       credentials: 'include'
     }).then(()=>{ setSuccess(true); handleTimeSuccessMessage() })
   }
+  function handleDeleteTypeProduct(e){
+    e.preventDefault()
+    const array = Array.from(e.target)
+    array.pop()
+    const values = array.map((input)=> input.value)
+    console.log(values)
+    // fetch('http://localhost:3000/protected/')
+  }
 
   useEffect(()=>{
     fetch('http://localhost:3000/api/getAllColors')
     .then((res)=>res.json())
     .then((data)=>{ setListColors(data) })
+    fetch('http://localhost:3000/api/getTypeProduct')
+    .then((res)=>res.json()).then((data)=>{ setListTypeProduct(data) })
   }, [])
-  
+
   return (
     <>
       {
@@ -38,13 +49,26 @@ function SectionDeleteRegisters(){
       }
       <SectionGeneric>
         <TitleBasic text="Borrar color"/>
-        <FormBasic handleSubmit={handleDelete}>
+        <FormBasic handleSubmit={handleDeleteColor}>
           <SelectLabelLeft 
             labelText='Seleccionar color' 
             options={ listColors }
             optionValueIsId={true}
             objContentKey="color_name"
             objIdKey="id_color"
+          />
+          <InputSubmit text="Borrar"/>
+        </FormBasic>
+      </SectionGeneric>
+      <SectionGeneric>
+        <TitleBasic text="Borrar tipo de producto"/>
+        <FormBasic handleSubmit={handleDeleteTypeProduct}>
+          <SelectLabelLeft 
+            labelText='Seleccionar color' 
+            options={ listTypeProduct }
+            optionValueIsId={true}  
+            objContentKey="type_product_name"
+            objIdKey="id_type_product"
           />
           <InputSubmit text="Borrar"/>
         </FormBasic>
